a482373ba8c7bb2f1cef14cdc48c5f2f929c4c20
AnYuan <hcyy@hc.com>
2025-02-26 14:26:39 +0800

    add qrcode login

---
+++ qrcodeLogIn/src/main/java/com/cnblog/qrcodeLogIn/QrcodeLogInApplication.java
@@ -0,0 +1,13 @@
+package com.cnblog.qrcodeLogIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeLogInApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeLogInApplication.class, args);
+    }
+    
+}
