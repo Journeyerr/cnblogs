c687663bc0f60af2b18a6831fdda7df88d0de032
AnYuan <hcyy@hc.com>
2025-02-26 14:26:33 +0800

    add qrcode login

---
+++ qrcodeLogIn/src/test/java/com/cnblog/qrcodeLogIn/QrcodeLogInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnblog.qrcodeLogIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeLogInApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
